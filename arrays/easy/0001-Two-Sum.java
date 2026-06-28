6e7a8404366e6fdc89f12b7b1080d6b510c6fac9
Arnab Das <arnobdas1391995@gmail.com>
2026-06-28 15:08:53 +0530

    Add Two Sum solution using HashMap

---
+++ arrays/easy/0001-Two-Sum.java
@@ -0,0 +1,25 @@
+/*
+Problem: 1. Two Sum
+
+Approach:
+Use a HashMap to store previously seen numbers.
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap <Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)){
+                return new int[] {map.get(complement), i};
+
+            }
+            map.put(nums[i], i);
+
+        }
+        return new int[] {};
+    }
+}
