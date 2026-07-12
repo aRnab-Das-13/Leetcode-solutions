c74f77f6b8dcb5cbcba1915b5ed7ab311e384937
Arnab Das <arnobdas1391995@gmail.com>
2026-07-12 23:05:33 +0530

    Solved Leetcode problem 453 Minimum moves to equal array elements

---
+++ arrays/medium/0453-Minimum-Moves-To-Equal-Array-Elements.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minMoves(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        for(int i : nums){
+            min = Math.min(min, i);
+        }
+        int result = 0;
+        for(int i = 0 ; i < nums.length; i++){
+            result += nums[i] - min;
+        }
+        return result;
+    }
+}
