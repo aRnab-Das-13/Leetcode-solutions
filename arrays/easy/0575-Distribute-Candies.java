d5c5a0dc678fda36a2bdb32074af18e436c0aa7d
Arnab Das <arnobdas1391995@gmail.com>
2026-08-02 19:59:23 +0530

    Solved leetcode problem 575 Distribute candies

---
+++ arrays/easy/0575-Distribute-Candies.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int candy : candyType){
+            set.add(candy);
+        }
+        if(set.size() >= candyType.length / 2){
+            return candyType.length / 2;
+        } else {
+            return set.size();
+        }
+        
+    }
+}
