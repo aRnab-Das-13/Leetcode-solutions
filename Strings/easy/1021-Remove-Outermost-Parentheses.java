eb5daba809ac73a89a242417cb67abf0ad24a6ec
Arnab Das <arnobdas1391995@gmail.com>
2026-08-21 12:47:13 +0530

    Solved leetcode problem 1021 Remove outermost parentheses

---
+++ Strings/easy/1021-Remove-Outermost-Parentheses.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        int count = 0;
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == '(') {
+                if (count != 0) {
+                    ans.append(s.charAt(i));
+                }
+                count++;
+            }
+
+            if (s.charAt(i) == ')') {
+                count--;
+
+                if (count != 0) {
+                    ans.append(s.charAt(i));
+                }
+            }
+        }
+
+        return ans.toString();
+    }
+}
